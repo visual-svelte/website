356af0884da6aa9322d2e2d7ac7f46826f437abb
ambim <will.needham@gmx.com>
2022-03-10 16:12:31 -0500

    holding page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <div class="inner">
+    <h1>Welcome to Visual Svelte</h1>
+    <p>Lots of luscious content coming soon!</p>
+  </div>
+</div>
+
+<style>
+  .container {
+    position: relative;
+    width: 100%;
+  }
+
+  .inner {
+    padding: 10% 0%;
+    max-width: 400px;
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1,
+  p {
+    position: relative;
+  }
+</style>
