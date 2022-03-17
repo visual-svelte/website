6972e7e1e988521e8c0a77107cdd7bb025073ae4
ambim <will.needham@gmx.com>
2022-03-17 00:30:10 -0500

    removed fake data

--- src/data/cms.js
+++ src/data/cms.js
@@ -19,39 +19,6 @@ let d3CMS = [
         props: { margin: 40, svg_height: 300, svg_width: 300 },
         component: AxisSimple,
       },
-      {
-        id: "AxisSimple",
-        title: "Simple D3 Axis",
-        notes:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quod omnis enim quae, obcaecati sed a officiis sit nesciunt blanditiis consequatur, pariatur ipsa quidem ipsam velit porro? Porro, modi molestias.",
-        props: { margin: 40, svg_height: 300, svg_width: 400 },
-        component: AxisSimple,
-      },
-      {
-        id: "AxisSimple",
-        title: "Simple D3 Axis",
-        notes:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quod omnis enim quae, obcaecati sed a officiis sit nesciunt blanditiis consequatur, pariatur ipsa quidem ipsam velit porro? Porro, modi molestias.",
-        props: { margin: 40, svg_height: 300, svg_width: 400 },
-        component: AxisSimple,
-      },
-      {
-        id: "AxisSimple",
-        title: "Simple D3 Axis",
-        notes:
-          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt quod omnis enim quae, obcaecati sed a officiis sit nesciunt blanditiis consequatur, pariatur ipsa quidem ipsam velit porro? Porro, modi molestias.",
-        props: { margin: 40, svg_height: 300, svg_width: 400 },
-        component: AxisSimple,
-      },
-      // {
-      //   id: "D3AxisLog",
-      //   title: "Logarithmic D3 Axis",
-      //   notes: "Lorem dipsum goes here",
-      //   props: { margin: 40, svg_height: 300, svg_width: 400 },
-      //   component: AxisDifferent,
-      //   github:
-      //     "https://api.github.com/repos/visual-svelte/website/contents/src/components/d3/AxisSimple.svelte",
-      // },
     ],
   },
   {
