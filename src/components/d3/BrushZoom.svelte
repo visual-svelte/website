e1b0a542600afed6bc4f0072f8a4b6f66dc7bd7c
ambim <will.needham@gmx.com>
2022-03-27 01:09:05 -0500

    brushZoom nearly working (interactions tbc)

--- src/components/d3/BrushZoom.svelte
+++ src/components/d3/BrushZoom.svelte
@@ -4,26 +4,58 @@
   import { areaData } from "$stores/brush";
   import _ from "lodash";
   let mySvg;
-  let width = 400;
-  let height = 300;
   let svg;
   let focus;
   let context;
   let bounds = [0, 0];
   let margin = { top: 20, right: 20, bottom: 110, left: 40 };
-  let margin2 = { top: 430, right: 20, bottom: 30, left: 40 };
-
-  $: width = +svg.attr("width") - margin.left - margin.right;
-  $: height = +svg.attr("height") - margin.top - margin.bottom;
-  $: height2 = +svg.attr("height") - margin2.top - margin2.bottom;
-  $: x = d3.scaleTime().range([0, width]);
-  $: x2 = d3.scaleTime().range([0, width]);
-  $: y = d3.scaleLinear().range([height, 0]);
-  $: y2 = d3.scaleLinear().range([height2, 0]);
+  let margin2 = { top: 330, right: 20, bottom: 30, left: 40 };
+  let pinxAxis;
+  let pinx2Axis;
+  let pinyAxis;
+  let brushHere;
+  let zoomHere;
+  $: width = 500 - margin.left - margin.right;
+  $: height = 400 - margin.top - margin.bottom;
+  $: height2 = 400 - margin2.top - margin2.bottom;
+  $: x = d3
+    .scaleTime()
+    .domain(
+      d3.extent($areaData, function (d) {
+        return d.date;
+      })
+    )
+    .range([0, width]);
+  $: x2 = d3.scaleTime().domain(x.domain()).range([0, width]);
+  $: y = d3
+    .scaleLinear()
+    .domain([
+      0,
+      d3.max($areaData, function (d) {
+        return d.price - 1;
+      }),
+    ])
+    .range([height, 0]);
+  $: y2 = d3.scaleLinear().domain(y.domain()).range([height2, 0]);
 
   $: xAxis = d3.axisBottom(x);
+  $: if (pinxAxis) {
+    d3.select(pinxAxis)
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+  }
   $: xAxis2 = d3.axisBottom(x2);
+  $: if (pinx2Axis) {
+    d3.select(pinx2Axis)
+      .attr("transform", "translate(0," + height2 + ")")
+      .call(xAxis2);
+  }
+
   $: yAxis = d3.axisLeft(y);
+  $: if (pinyAxis) {
+    d3.select(pinyAxis).call(yAxis);
+  }
+
   $: brush = d3
     .brushX()
     .extent([
@@ -31,6 +63,10 @@
       [width, height2],
     ])
     .on("brush end", brushed);
+
+  $: if (brushHere) {
+    d3.select(brushHere).call(brush).call(brush.move, x.range());
+  }
 
   $: area = d3
     .area()
@@ -54,6 +90,27 @@
       return y2(d.price);
     });
 
+  $: a1 = area($areaData);
+  $: a2 = area2($areaData);
+  $: console.log("a1: ", a1, "a2: ", a2);
+
+  $: zoom = d3
+    .zoom()
+    .scaleExtent([1, Infinity])
+    .translateExtent([
+      [0, 0],
+      [width, height],
+    ])
+    .extent([
+      [0, 0],
+      [width, height],
+    ])
+    .on("zoom", zoomed);
+
+  $: if (zoomHere) {
+    d3.select(zoomHere).call(zoom);
+  }
+
   function brushed(event) {
     if (event.sourceEvent && event.sourceEvent.type === "zoom") return; // ignore brush-by-zoom
     let s = event.selection || x2.range();
@@ -68,41 +125,88 @@
       );
   }
 
-  function drawAxis() {
-    svg = d3.select(mySvg);
-    svg.append("g").attr("class", "brush").call(brush);
-    focus = svg
-      .append("g")
-      .attr("class", "focus")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    context = svg
-      .append("g")
-      .attr("class", "context")
-      .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+  function zoomed(event) {
+    if (event.sourceEvent && event.sourceEvent.type === "brush") return; // ignore zoom-by-brush
+    let t = event.transform;
+    x.domain(t.rescaleX(x2).domain());
+    focus.select(".area").attr("d", area);
+    focus.select(".axis--x").call(xAxis);
+    context.select(".brush").call(brush.move, x.range().map(t.invertX, t));
   }
 
+  function drawAxis() {
+    svg = d3.select(mySvg);
+    svg
+      .append("defs")
+      .append("clipPath")
+      .attr("id", "clip")
+      .append("rect")
+      .attr("width", width)
+      .attr("height", height);
+  }
   onMount(() => {
     drawAxis();
   });
 </script>
 
-<!-- <p>Original: {$brushData.length}</p> -->
-<!-- <p>Filtered: {$filtered.length}</p> -->
-<!-- <p>Bounds: {bounds[0].toPrecision(4)}, {bounds[1].toPrecision(4)}</p> -->
-
-<svg bind:this={mySvg} width="500" height="960" />
+<svg bind:this={mySvg} width="500" height="400">
+  <g
+    bind:this={focus}
+    class="focus"
+    style="transform:translate({margin.left}px,{margin.top}px)"
+  >
+    <path d={area($areaData)} class="area" />
+    <g class="axis axis--x" bind:this={pinxAxis} />
+    <g class="axis axis--y" bind:this={pinyAxis} />
+  </g>
+  <g
+    bind:this={context}
+    class="context"
+    style="transform:translate({margin2.left}px,{margin2.top}px);"
+  >
+    <g class="brush" bind:this={brushHere} />
+    <g class="axis axis--x" bind:this={pinx2Axis} />
+    <path d={area2($areaData)} class="area" />
+  </g>
+  <rect
+    bind:this={zoomHere}
+    class="zoom"
+    {width}
+    {height}
+    style="transform:translate({margin.left}px,{margin.top}px)"
+  />
+</svg>
 
 <style>
   .selected,
   .not-selected {
     transition: 0.5s all;
   }
+
+  .context {
+    transform: translate(20, 420);
+  }
   .circle,
   .bar {
     fill: var(--c-darkgray);
     fill-opacity: 0.7;
   }
+
+  .area {
+    fill: steelblue;
+    clip-path: url(#clip);
+    z-index: 1;
+  }
+
+  .zoom {
+    cursor: move;
+    fill: none !important;
+    pointer-events: all;
+  }
+  .brush {
+    z-index: 10 !important;
+  }
+
   .bar {
     fill: var(--c-darkgray);
     fill-opacity: 0.3;
--- src/stores/brush.js
+++ src/stores/brush.js
@@ -1,6 +1,7 @@
 import * as d3 from "d3";
 import { readable, writable } from "svelte/store";
-import { areaDataCsv } from "$data/areaData.csv";
+import areaDataCsv from "$data/areaData.csv";
+let parseDate = d3.timeParse("%b %Y");
 
 var data = d3.range(100).map(() => {
   let x = d3.randomUniform(0, 400)();
@@ -8,6 +9,15 @@ var data = d3.range(100).map(() => {
   return [x, y];
 });
 
+function type(d) {
+  d.date = parseDate(d.date);
+  d.price = +d.price;
+  return d;
+}
+
+const finalData = areaDataCsv.map((d) => {
+  return { date: parseDate(d.date), price: d.price };
+});
 export const brushData = readable(data);
 export const filtered = writable(data);
-export const areaData = writable(areaDataCsv);
+export const areaData = writable(finalData);
