44e83934cd62d153fecb08264a395ac5d407a95e
ambim <will.needham@gmx.com>
2022-04-04 01:26:54 -0500

    removed unnecessary text

--- .netlify/functions-internal/render.js
+++ .netlify/functions-internal/render.js
@@ -5,7 +5,7 @@ export const handler = init({
 	assets: new Set(["dataviz.gif","favicon.png","fonts/CONSOLA.TTF","fonts/CONSOLAB.TTF","fonts/consolai.ttf","fonts/Consolas.ttf","fonts/consolaz.ttf","images/favicon/android-chrome-192x192.png","images/favicon/android-chrome-512x512.png","images/favicon/apple-touch-icon.png","images/favicon/favicon-16x16.png","images/favicon/favicon-32x32.png","images/favicon/favicon.ico","images/thumbnails/brush.png","images/thumbnails/chord.png","images/thumbnails/contour.png","images/thumbnails/drag.png","images/thumbnails/force.png","images/thumbnails/geo.png","images/thumbnails/graph.png","images/thumbnails/hierarchy.png","images/thumbnails/polygon.png","images/thumbnails/shapes.png","images/thumbnails/zoom.png"]),
 	_: {
 		mime: {".gif":"image/gif",".png":"image/png",".TTF":"font/ttf",".ttf":"font/ttf",".ico":"image/vnd.microsoft.icon"},
-		entry: {"file":"start-fc38525c.js","js":["start-fc38525c.js","chunks/vendor-43ccc58c.js","chunks/singletons-d1fb5791.js"],"css":["assets/vendor-0cdc4f15.css"]},
+		entry: {"file":"start-4210e232.js","js":["start-4210e232.js","chunks/vendor-43ccc58c.js","chunks/singletons-d1fb5791.js"],"css":["assets/vendor-0cdc4f15.css"]},
 		nodes: [
 			() => import('../server/nodes/0.js'),
 			() => import('../server/nodes/1.js'),
